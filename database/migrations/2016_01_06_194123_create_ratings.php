96ea2ac562ede6a54526cfac3e31bdb776621e7e
Charlie King <charlie.king@clearlink.com>
2016-01-06 12:50:27 -0700

    Ratings table

---
+++ database/migrations/2016_01_06_194123_create_ratings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('movie_id')->unsigned();
+            $table->integer('rating')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ratings');
+    }
+}
