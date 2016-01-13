9f4c4389da392587e6d5d9875982e752d9f76f2c
maxpowerh <maxpowerh@users.noreply.github.com>
2016-01-12 18:50:41 -0700

    migrations

--- database/migrations/2016_01_13_011347_media_type.php
+++ database/migrations/2016_01_13_011347_media_type.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Media extends Migration
+class MediaType extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,10 @@ class Media extends Migration
      */
     public function up()
     {
-        Schema::create('media', function (Blueprint $table) {
+        Schema::create('media_type', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->integer('type_id')->unsigned();
+            $table->string('type');
+            $table->integer('active')->default(1);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ class Media extends Migration
      */
     public function down()
     {
-        Schema::drop('media');
+        Schema::drop('media_type');
     }
 }
