50b2bfd3d8d508a5606659938c1c1fa87e00ed20
Jason Wilkin <jason@wilk.in>
2016-02-03 15:33:12 -0700

    bootstrap slider includes

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -1,3 +1,4 @@
 <script src="{{ URL::asset('assets/js/jquery-2.2.0.min.js') }}"></script>
 <script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/bootstrap-slider.js') }}"></script>
 <script src="{{ URL::asset('assets/js/main.js') }}"></script>
