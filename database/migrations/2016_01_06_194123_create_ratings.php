5abc6c76ee956828d6f4867238397fae32a8c736
maxpowerh <maxpowerh@users.noreply.github.com>
2016-03-02 22:18:01 -0700

    Database rate page migration changes

--- database/migrations/2016_01_06_194123_create_ratings.php
+++ database/migrations/2016_01_06_194123_create_ratings.php
@@ -17,6 +17,16 @@ class CreateRatings extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('movie_id')->unsigned();
             $table->integer('rating')->unsigned();
+            $table->date('date_watched');
+            $table->integer('directing')->unsigned();
+            $table->integer('leading_actors')->unsigned();
+            $table->integer('supporting_cast')->unsigned();
+            $table->integer('music')->unsigned();
+            
+    //I think we should discuss these last three or change how they work in the futrue
+            $table->integer('experience')->unsigned();
+            $table->integer('mood')->unsigned();
+            $table->integer('watched_with')->unsigned();
             $table->timestamps();
         });
     }
