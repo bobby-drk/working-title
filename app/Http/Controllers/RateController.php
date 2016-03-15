7be48f122d6382025009ffb877aae4c64ee95087
Jason Wilkin <jason@wilk.in>
2016-03-15 15:27:46 -0600

    Rate Controller changes

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -15,23 +15,29 @@ class RateController extends Controller
     public function newrating()
     {
         $my_id = Auth::id();
+        $score = Input::get('score');
+        $date = Input::get('date');
+        $directing = Input::get('directing');
+        $lead_actors = Input::get('lead_actors');
+        $supporting_cast = Input::get('supporting_cast');
+        $music = Input::get('music');
+        $experience = Input::get('experience');
+        $mood = Input::get('mood');
+        $with = Input::get('with');
+        
         $new_rating = new Rate ();
         $new_rating->user_id = $my_id;
-        $new_rating->movie_id = 4;
-        $new_rating->rating = 'score';
-        $new_rating->save();
-        
-//        Variables from the page:
-//        score
-//        date
-//        directing
-//        lead_actors
-//        supporting_cast
-//        other
-//        experience
-//        mood
-//        with
-//        
+        $new_rating->movie_id = 42;
+        $new_rating->rating = $score;
+        $new_rating->date_watched = $score;
+        $new_rating->directing = $directing;
+        $new_rating->leading_actors = $lead_actors;
+        $new_rating->supporting_cast = $supporting_cast;
+        $new_rating->music = $music;
+        $new_rating->experience = $experience;
+        $new_rating->mood = $mood;
+        $new_rating->with = $with;        
+
         Alert::add("You rated a move successfully!");
         return redirect()->route('rate');
     }
